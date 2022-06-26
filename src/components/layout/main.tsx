f71b028c597bf99126fd649dfe0f3b89ca002a5e
skidrow91 <vanhan91@gmail.com>
2022-06-27 00:12:58 +0700

    first updated

---
+++ src/components/layout/main.tsx
@@ -0,0 +1,13 @@
+import Header from './Header/header';
+
+function Main() {
+
+	return (
+		<div>
+			<Header />
+			<h5>Test3423423</h5>
+		</div>
+	);
+}
+
+export default Main;
---
+++ src/views/LoginForm/LoginForm.tsx
@@ -0,0 +1,14 @@
+function LoginForm() {
+	return (
+		<div>
+			<form>
+				<label>Username</label>
+				<input type="text" value="" />
+				<label>Password</label>
+				<input type="text" value="password" />
+			</form>
+		</div>
+	);
+}
+
+export default LoginForm;
